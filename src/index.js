1d83f6ad668abf5d5ecd725f9ef5b0412879d6ce
Diana Liao <dijali@gmail.com>
2021-04-08 11:34:52 -0400

    basics

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  // const submitButton = document.querySelector("#create-task-form > input[type=submit]")
+  // submitButton.addEventListener("click", console.log(event))
+  
+  const taskForm = document.querySelector("form#create-task-form")
+  const taskList = document.querySelector("ul#tasks")
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    postTask()
+    console.log("form form boop")
+  })
+  
+  function postTask(){
+    newTaskText = taskForm[0].value
+    newTask = document.createElement("li")
+    newTask.textContent = newTaskText
+
+    taskList.append(newTask)
+  }
+
+
 });
+
+
